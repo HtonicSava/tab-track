dc28c2a040d3b4fb516367a82914018bc29a4e21
HtonicSava <htonic.sava@gmail.com>
2021-07-06 22:31:10 +0300

    server init

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+
+const app = express()
+app.use(morgan('combined'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(cors())
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'Hello world'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
